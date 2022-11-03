2018bc53b3894cd81266885976bed1fd318a5e1f
Anna Khablo <85759163+khablo1508@users.noreply.github.com>
2022-11-03 14:28:35 +0100

    remove irrelevant globals

--- js/script.js
+++ js/script.js
@@ -4,9 +4,7 @@ Globals
 ================
 */
 const popup = document.querySelector('.popup');
-const winner = document.querySelector('.winner');
 const gameStartButton = document.getElementById('game-start-button');
-const restartButton = document.getElementById('restart-button');
 const canvas = document.getElementById('canvas');
 
 /* 
